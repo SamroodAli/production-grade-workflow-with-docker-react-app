08a50f594a666e7261ce9d3620e2414aaff9725e
samroodAli <devsamrood@gmail.com>
2021-10-22 11:11:05 +0530

    Fix: Fix docker user to node to solve permission issues

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        hello world, finally, docker was hard, but i still dont know why i did it too
         </p>
         <a
           className="App-link"
